408bc5764ec564b9b372f96a001de619a1c95fdd
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-04-13 13:37:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['515474180603641866'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const ms = require("ms");
+const premium = ['515474180603641866']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
@@ -32,6 +33,29 @@ var table = require('table').table
 var ti={}  
 ,spee={}
 ,attentions={};
+
+
+client.on('message', message => {
+        
+    if(message.content.startsWith(prefix + 'وقت')) {
+          let args = message.content.split(' ').slice(1);
+   let Timer = args[0]; 
+ 
+   if(!args[0]){
+     return message.channel.send("من فضلك , قم بكتابتة بـ s,m,h "); 
+   }
+ 
+   if(message[0] <= 0){ 
+     return message.channel.send("من فضلك , قم بكتابتة بـ s,m,h ");  
+   }
+ 
+   message.channel.send(`**✅ | بدأ العدد الزمني : \`${ms(ms(Timer), {long: true})}\`.**`)  
+   setTimeout(function(){
+     message.channel.send(`** ${message.author} الوقت الزمني انتهى من :\`${ms(ms(Timer), {long: true})}\`.**`) 
+   }, ms(Timer)); 
+ 
+     } 
+ });
 
 
 client.login(process.env.BOT_TOKEN)
