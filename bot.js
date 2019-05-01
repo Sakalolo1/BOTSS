c08fc5bc71dd5a176c55c89ab35a58996c65d73a
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-01 04:41:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -320,14 +320,35 @@ client.on('message', message => {
 
 client.on('message', message => {
        if (message.content.startsWith(prefix + 'botserver')) {
-     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let msg =  client.guilds.map(guild => `**${guild.name} عدد الاعضاء:          ${guild.memberCount}**`).join('\n');
   let embed = new Discord.RichEmbed()
-  .setTitle(`${client.guilds.size}سيرفرات `)
-  .setDescription(`${msg}`)
+  .setTitle(`${client.guilds.size} **سيرفرات   **`)
+  .setDescription(`**${msg}   **`)
   .setColor("#ebf442");
   message.channel.send(embed);
 }
 });
+
+client.on("message", message => {
+let args = message.content.split(' ');
+  if(args[0].toLowerCase() === (prefix + "server")) {
+        let embed = new Discord.RichEmbed()
+        .addField(`:id: Server ID:`, `${message.guild.id}`, true)
+        .addField(`:calendar: Created on:`, `${moment(message.guild.createdAt).format(`D/M/YYYY h:mm`)} \n ${moment(message.guild.createdAt).locale("AR-eg").fromNow()}`,true)
+        .addField(`:crown: Owned by`, `${message.guild.owner.user.tag} [${message.guild.owner.user.id}]`,true) 
+        .addField(`:busts_in_silhouette: Members [${message.guild.members.size}]`, `**${message.guild.members.filter(c => c.presence.status !== "offline").size}** Online`, true)
+        .addField(`:speech_balloon: Channels [${message.guild.channels.size}]`,`**${message.guild.channels.filter(f => f.type === "text").size}** Text | **${message.guild.channels.filter(f => f.type === "voice").size}** Voice`,true)
+        .addField(`:earth_africa: Others`, `**Region:** ${message.guild.region} \n **Verification level:** ${message.guild.verificationLevel}`, true)  
+        .addField(`:closed_lock_with_key: Roles [${message.guild.roles.size}]`, `To see the whole list with all roles use **${prefix}roles**`, true) 
+        .setThumbnail(`${message.guild.iconURL}`)
+        .setColor(`black`)
+        .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`);
+       
+    message.channel.sendEmbed(embed);
+    }
+
+});
+
 
 client.on('message', msg =>{
         let args = msg.content.slice(4);
