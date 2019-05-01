4e701548be30f950a68b10f24d42d545bec05e76
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-01 04:07:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -285,5 +285,17 @@ client.on('message', async message => {
 });
 
 
+client.on('message', async message => {
+  let args = message.content.slice(3);
+  if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return message.channel.send('Required Administrator Permission')
+       message.guild.members.forEach(m => {
+      
+      m.send(args.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', message.author.username))
+    })
+  }
+})
+
+
 client.login(process.env.BOT_TOKEN)
 client2.login(process.env.BOT_TOKEN2)
