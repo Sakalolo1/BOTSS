916f3fdde4eb14be7321a41987ed6c3b61d97a92
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-01 03:29:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -311,6 +311,20 @@ if(m.roles.some(r => r.id == role.id)) return
 })
 
 
+var prefix = "D";
+
+client.on("message", async message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if(message.author.id != "515474180603641866") return;
+    if(message.author.bot) return;
+    if (command == "leave") {
+        if(!args[0] || args[1]) return message.reply(`**${prefix}leave <guild_id>**`);
+        let GuildId = client.guilds.get(args[0])
+        if(!GuildId) return message.reply(`** Guild ID is not Detected**`);
+        GuildId.leave().then(m => message.channel.send("**I have Left "+GuildId.name+" ✅**"))
+    }     
+})
 
 client.login(process.env.BOT_TOKEN)
 client2.login(process.env.BOT_TOKEN2)
