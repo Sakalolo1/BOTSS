cc46b4c70963478d7dfab77afaca1a31e9bf053e
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-02 13:44:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -383,6 +383,19 @@ client.on('guildCreate', guild => {
         guild.owner.send(embed)
   });
 
+client.on('message', message => {
+  if(message.content === prefix + "user"){
+    var embed = new Discord.RichEmbed()
+    .setTitle(message.author.tag, message.author.avatarURL)
+    .addField(`User`, message.author.username)
+    .addField(`Discrim`,`#`+ message.author.discriminator)
+    .addField(`Name Color Role`, message.member.colorRole)
+    .addField(`Game`,message.author.presence.game ||"Idel.")
+    .addField(`Status`,message.author.presence.status)
+    message.channel.send(embed);
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN)
 client2.login(process.env.BOT_TOKEN2)
