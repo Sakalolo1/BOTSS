07f46943d2840fc4505e906916465d1edad5252f
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-03 06:28:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -310,42 +310,13 @@ client.on('message', message => {
 }
 });
 
-client.on("message", message => {
-    let prefix= 'A'
-let args = message.content.split(' ');
-  if(args[0].toLowerCase() === (prefix + "server")) {
-        let embed = new Discord.RichEmbed()
-        .addField(`:id: Server ID:`, `${message.guild.id}`, true)
-        .addField(`:calendar: Created on:`, `${moment(message.guild.createdAt).format(`D/M/YYYY h:mm`)} \n ${moment(message.guild.createdAt).locale("AR-eg").fromNow()}`,true)
-        .addField(`:crown: Owned by`, `${message.guild.owner.user.tag} [${message.guild.owner.user.id}]`,true) 
-        .addField(`:busts_in_silhouette: Members [${message.guild.members.size}]`, `**${message.guild.members.filter(c => c.presence.status !== "offline").size}** Online`, true)
-        .addField(`:speech_balloon: Channels [${message.guild.channels.size}]`,`**${message.guild.channels.filter(f => f.type === "text").size}** Text | **${message.guild.channels.filter(f => f.type === "voice").size}** Voice`,true)
-        .addField(`:earth_africa: Others`, `**Region:** ${message.guild.region} \n **Verification level:** ${message.guild.verificationLevel}`, true)  
-        .addField(`:closed_lock_with_key: Roles [${message.guild.roles.size}]`, `To see the whole list with all roles use **${prefix}roles**`, true) 
-        .setThumbnail(`${message.guild.iconURL}`)
-        .setColor(`black`)
-        .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`);
-       
-    message.channel.sendEmbed(embed);
-    }
-
-});
-
-
-client.on('message', msg =>{
-        let args = msg.content.slice(4);
-  if (msg.author.id !== '515474180603641866') return;
-  if (msg.content.startsWith('Sweet')) {
-   msg.channel.send(args)   
-  }
-})
 
 
 client.on('message', message => {
   if(message.content.includes('discord.gg/')){                                       
       if (!message.member.hasPermissions(['ADMINISTRATOR'])){
       message.delete()
-  return message.reply(`** Not allowed to advertising Here :angry: ! **`)
+  return message.reply(`** لا يسمح لدعاية هنا  :angry: ! **`)
   }
 }
 });
