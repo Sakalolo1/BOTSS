9ba54129315b2b8e5aa08b71824c98f1d6275020
Sakalolo1 <49027135+Sakalolo1@users.noreply.github.com>
2019-05-03 06:25:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,22 +138,6 @@ if(!message.channel.guild) return message.channel.send('**هذا الأمر فق
 });
 
 
-
-client.on("guildCreate" , guild => {
-    const embed = new Discord.RichEmbed()
-        .setTimestamp()
-        .setAuthor(`${guild.name} (${guild.id})`)
-        .addField(`Owner`, `${guild.owner.user.tag} (${guild.ownerID})`)
-        .addField(`Channels`, `${guild.channels.size}`)
-        .addField(`Members`, `${guild.memberCount}`)
-        .addField(`Servers`, `${client.guilds.size}`)
-        .setColor('GREEN')
-        .setFooter('JOINED DISCORD SERVER');
-    client.channels.get('573084977730682890').send({
-        embed: embed
-    });
-})
-
 client.on('message', msg => {
   if(msg.author.bot) return
   if(msg.content.startsWith(prefix + 'role')) {
@@ -391,67 +375,6 @@ client.on('message', message => {
     .addField(`Status`,message.author.presence.status)
     message.channel.send(embed);
   }
-});
-
-
-client.on('guildCreate', guild => {
-  let support = client.guilds.get('569579700133953547') // حط هنا ايدي سيرفر السبورت
-  if(support === undefined) return
-  let role = support.roles.find(r => r.name == 'Tester.') // بدلها بأسم الرتبة يلي تبيها للمستخدمين
-  let member = support.members.get(guild.owner.user.id) 
-  if(member) {
-    member.addRole(role)
-  } else {
-    console.log(`this user not in support server`)
-  }
-})
-
-
-
-
-
-
-
-client.on("message", message => {
-    if(message.content.startsWith(prefix + "server")){
-        if(message.author.bot || message.channel.type == "dm") return;
-        let onlineM = message.guild.members.filter(m => m.presence.status !== "offline");
-        let verifyL = ["None", "Low", "Medium", "Hard", "Extreme"];
-        let region = {
-            'brazil': "`Brazil`",
-            'eu-central': "`Central Europe`",
-            'singapore': "`Singapore`",
-            'us-central': "`US Central`",
-            'sydney': "`Sydney`",
-            'us-east': "`US East`",
-            'us-south': "`US South`",
-            'us-west': "`US West`",
-            'eu-west': "`Western Europe`",
-            'london': "`London`",
-            'amsterdam': "`Amsterdam`",
-            'hongkong': "`Hong Kong`",
-            'russia': "`Russia`"
-        };
-        let serverEmbed = new Discord.RichEmbed()
-        .setColor("#36393e")
-        .setThumbnail(message.guild.iconURL)
-        .setAuthor(`${message.guild.name}, Server Info`)
-        .setDescription(`- Server Name \`${message.guild.name}\`\n- Created At \`${moment(message.guild.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Total Members \`${message.guild.memberCount} [Online: ${onlineM.size}]\`\n- Server Owner \`${message.guild.owner.user.tag}\`\n- Channels \`${message.guild.channels.filter(m => m.type == 'text').size} Text || ${message.guild.channels.filter(m => m.type == 'voice').size} Voice\`\n- Categories \`${message.guild.channels.filter(m => m.type == 'category').size}\`\n- Roles \`${message.guild.roles.size}\`\n- Region \`${region[message.guild.region]}\`\n- Verification Level \`${verifyL[message.guild.verificationLevel]}\`\n- Server ID \`${message.guild.id}\``)
-        .setFooter(message.client.user.username,message.client.user.avatarURL);
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`:no_entry: You dont have permission!`).then(mm => mm.delete(5000));  message.channel.send(serverEmbed).catch(console.error);
-    } else if(message.content.startsWith(prefix + "user")){
-        if(message.author.bot || message.channel.type == "dm") return;
-        let mnt = message.mentions.users.first();
-        let user = mnt || message.author;
-        let userEmbed = new Discord.RichEmbed()
-        .setColor("#36393e")
-        .setThumbnail(user.avatarURL)
-        .setAuthor(`${user.username}, User Info`)
-        .setDescription(`- Name \`${user.tag}\`\n- Created At \`${moment(user.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Joined At \`${moment(user.joinedAt).format('D/MM/YYYY h:mm a')}\`\n- ID \`${user.id}\``)
-        .setFooter(message.client.user.username,message.client.user.avatarURL);
-        message.channel.send(userEmbed).catch(console.error);
-       
-    }
 });
 
 
